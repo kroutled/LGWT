df68724c8cacef71e2bbc99eb8be826a5881a84e
kroutled <kyle.routledge1@gmail.com>
2025-01-21 21:27:11 +0200

    test refactor

--- hello_test.go
+++ hello_test.go
@@ -8,17 +8,18 @@ func TestHello(t *testing.T){
     t.Run("Greeting person by name", func(t *testing.T){
         got := Hello("Kyle")
         want := "Hello, Kyle"
-
-        if got != want {
-            t.Errorf("got: %q want: %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
     t.Run("Testing greeting with no name", func(t *testing.T){
         got := Hello("")
         want := "Hello World"
+        assertCorrectMessage(t, got, want)
+    })
+}
 
-        if got != want {
-            t.Errorf("got: %q want: %q", got, want)
-        }
-    }) 
+func assertCorrectMessage(t testing.TB, got, want string){
+    t.Helper()
+    if got != want {
+        t.Errorf("got: %q want: %q", got, want)
+    }
 }
