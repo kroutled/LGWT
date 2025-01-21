3db5c54c654be9af5a724b9cb7b38dafdcaafbb0
kroutled <kyle.routledge1@gmail.com>
2025-01-21 22:01:46 +0200

    some updates to language greetings and tests

--- hello.go
+++ hello.go
@@ -5,15 +5,26 @@ import (
 )
 
 const englishGreet = "Hello, "
+const spanishGreet = "Hola, "
+const frenchGreet = "Bonjour, "
+const sp = "Spanish"
+const en = "English"
+const fr = "French"
 
-func Hello(name string) string{
+func Hello(name, language string) string{
     greet := englishGreet + name
     if name == "" {
         greet = "Hello World"
+    }
+    if language == sp {
+        greet = spanishGreet + name
+    }
+    if language == fr {
+        greet = frenchGreet + name
     }
     return greet
 }
 
 func main(){
-    fmt.Println(Hello("Kyle"))
+    fmt.Println(Hello("Kyle", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -6,13 +6,23 @@ import(
 
 func TestHello(t *testing.T){
     t.Run("Greeting person by name", func(t *testing.T){
-        got := Hello("Kyle")
+        got := Hello("Kyle", "")
         want := "Hello, Kyle"
         assertCorrectMessage(t, got, want)
     })
     t.Run("Testing greeting with no name", func(t *testing.T){
-        got := Hello("")
+        got := Hello("", "")
         want := "Hello World"
+        assertCorrectMessage(t, got, want)
+    })
+    t.Run("Testing Spanish input", func(t *testing.T){
+        got := Hello("Kail", "Spanish")
+        want := "Hola, Kail"
+        assertCorrectMessage(t, got, want)
+    })
+    t.Run("Testing French Input", func(t *testing.T){
+        got := Hello("Kyle", "French")
+        want := "Bonjour, Kyle"
         assertCorrectMessage(t, got, want)
     })
 }
